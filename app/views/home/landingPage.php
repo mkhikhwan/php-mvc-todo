4d1251f8b4239a36fab5c38c3401f4172d993a99
mkhikhwan <mkhikhwan@gmail.com>
2025-12-09 17:21:28 +0800

    fix: landing page hero banner

--- app/views/home/landingPage.php
+++ app/views/home/landingPage.php
@@ -1,3 +1,24 @@
-<h1>Welcome to PHP Todo.</h1>
-<p>This is a simple To-Do List App built with vanilla PHP. To Start, Click the button Below!</p>
-<button>Start</button>
+<!-- Hero Banner -->
+<div class="hero-banner">
+    <!-- Left Side -->
+    <div class="hero-left-side">
+        <div class="hero-left-side-content">
+            <h1>PHP MVC To-Do App</h1>
+            <h3>
+                A simple, handcrafted task manager built from scratch to explore core concepts like routing, controllers, and clean MVC architecture. 
+                Learn, experiment, and see how everything fits together—one feature at a time.
+            </h3>
+            <a href="/tasks" class="btn btn-primary">Let's Start</a>
+        </div>
+    </div>
+
+    <!-- Right Side -->
+    <div class="hero-right-side">
+        Right Side
+    </div>
+</div>
+
+<div class="features-section">
+
+</div>
+<!-- Explain the Features -->
