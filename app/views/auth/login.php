7b990d31058e4a791a93da62a8b64e83138613fa
mkhikhwan <mkhikhwan@gmail.com>
2025-12-11 16:29:58 +0800

    fix: login page styling

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -1,18 +1,28 @@
-<h2>Login</h2>
+<div class="auth-container">
+    <form method="post" action="/login" class="auth-form">
+        <h1>Login</h1>
 
-<?php if (!empty($errors['general'])): ?>
-    <p class="error"><?php echo htmlspecialchars($errors['general']); ?></p>
-<?php endif; ?>
+        <?php if (!empty($errors['general'])): ?>
+            <p style="text-align:center; color: red; font-style:italic;">*<?php echo htmlspecialchars($errors['general']); ?></p>
+            <br>
+        <?php endif; ?>
 
-<form method="post" action="/login">
-    <div>
-        <label for="username">Username</label>
-        <input type="text" name="username" value="<?php echo htmlspecialchars($old['username'] ?? ''); ?>">
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input type="password" name="password">
-    </div>
+        <div class="auth-input-group">
+            <input type="text" name="username" 
+                   value="<?php echo htmlspecialchars($old['username'] ?? ''); ?>" 
+                   placeholder="Username">
+            <label for="username">Username</label>
+        </div>
 
-    <input type="submit" value="Register">
-</form>
+        <div class="auth-input-group">
+            <input type="password" name="password" placeholder="Password">
+            <label for="password">Password</label>
+        </div>
+
+        <div class="auth-message">
+            Don't have an account? <a href="/register">Register here.</a>
+        </div>
+
+        <button type="submit" class="auth-submit">Login</button>
+    </form>
+</div>
