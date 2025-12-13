1d0ff9e39377e65e9feba8bd426c6332034920d8
mkhikhwan <mkhikhwan@gmail.com>
2025-12-14 01:49:54 +0800

    feat: session time

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 session_start();
+$timeout_duration = 1800; // 30 Minutes of Session time
+
 require_once 'app/core/Router.php';
 require_once 'app/core/Database.php';
 require_once 'app/core/routes.php';
@@ -7,6 +9,18 @@ require_once 'app/core/HTTPException.php';
 require_once 'app/core/Controller.php';
 
 try{
+    if(
+        isset($_SESSION['LAST_ACTIVITY']) && 
+        (time() - $_SESSION['LAST_ACTIVITY']) > $timeout_duration
+    ){
+        // Log user out after session expires
+        session_unset();
+        session_destroy();
+        header("Location: /");
+        exit();
+    } 
+
+    $_SESSION['LAST_ACTIVITY'] = time();
     $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
 }catch(HTTPException $e){
     $code = $e->getStatusCode();
